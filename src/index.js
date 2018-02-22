a20a23064dc8714b201b8a4a20d3adfbc0d11eff
Raman Trus <radadendronroma@gmail.com>
2018-02-22 21:25:27 +0300

    looks like it almost work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var i = 2;
+	var factors = [];
+	while (base > 0 && i <= base) {
+		while (base % i == 0) {
+			factors.push(i);
+			base = base / i;
+		}
+		i++;
+	}
+  factors.sort((a,b)=>{return a-b})
+  let counter=0;
+  while (number>0){
+  number=Math.floor(number/factors[factors.length-1])
+  counter+=number
+}
+return counter
 }
